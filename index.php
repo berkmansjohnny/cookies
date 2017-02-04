378c6058d3532cd0d6cb7400adff8d420d83793a
kingjohnny <johnny@thinktomorrow.be>
2017-02-04 20:35:01 +0100

    Initial setup

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+
+
+	<!-- search meta -->
+	<title> Cookies.css </title>
+	<meta name="author" content="[]" />
+	<meta name="description" content="[]" />
+	<meta name="keywords" content="[]" />
+	<meta name="robots" contents="[]" />
+
+	<!-- styles -->
+	<link rel="stylesheet" href="assets/css/cookies.css" />
+
+	<!-- icons -->
+	<link rel="shortcut icon" href="cookie.ico" />
+	<link rel="apple-touch-icon-precomposed" href="assets/img/apple-touch-icon-precomposed.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/img/apple-touch-icon-72x72-precomposed.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/img/apple-touch-icon-114x114-precomposed.png" />
+
+	<!-- head scripts -->
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+	<!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+</head>
+
+
+<body>
+<header>
+	<h1>Cookies.css</h1>
+</header>
+<section class="first">
+	<div class="cookie bar">
+		<p>Dit is een cookiebar</p>
+		<a href="#"></a>
+	</div>
+</section>
+
+
+
+	<footer>
+		<small> &copy; <?php echo(date('Y')) ?> </small>
+	</footer>
+
+
+	<!-- foot scripts -->
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>
