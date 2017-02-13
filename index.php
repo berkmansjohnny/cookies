6fd3dfcd0c88552fdb22013e45180ac629d0a49f
kingjohnny <johnny@thinktomorrow.be>
2017-02-13 21:17:56 +0100

    Update: cookies

--- index.php
+++ index.php
@@ -6,6 +6,20 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta name="viewport" content="width=device-width,initial-scale=1" />
 
+<!--
+▄████▄   ▒█████   ▒█████   ██ ▄█▀ ██▓▓█████   ██████
+▒██▀ ▀█  ▒██▒  ██▒▒██▒  ██▒ ██▄█▒ ▓██▒▓█   ▀ ▒██    ▒
+▒▓█    ▄ ▒██░  ██▒▒██░  ██▒▓███▄░ ▒██▒▒███   ░ ▓██▄
+▒▓▓▄ ▄██▒▒██   ██░▒██   ██░▓██ █▄ ░██░▒▓█  ▄   ▒   ██▒
+▒ ▓███▀ ░░ ████▓▒░░ ████▓▒░▒██▒ █▄░██░░▒████▒▒██████▒▒
+░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒░▓  ░░ ▒░ ░▒ ▒▓▒ ▒ ░
+ ░  ▒     ░ ▒ ▒░   ░ ▒ ▒░ ░ ░▒ ▒░ ▒ ░ ░ ░  ░░ ░▒  ░ ░
+░        ░ ░ ░ ▒  ░ ░ ░ ▒  ░ ░░ ░  ▒ ░   ░   ░  ░  ░
+░ ░          ░ ░      ░ ░  ░  ░    ░     ░  ░      ░
+░
+
+-->
+
 
 	<!-- search meta -->
 	<title> Cookies.css </title>
@@ -18,6 +32,7 @@
 	<link rel="stylesheet" href="assets/css/normalize.css" />
 	<link rel="stylesheet" href="assets/css/cookies.css" />
 	<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 	<!-- icons -->
 	<link rel="shortcut icon" href="cookie.ico" />
 	<link rel="apple-touch-icon-precomposed" href="assets/img/apple-touch-icon-precomposed.png" />
@@ -34,9 +49,15 @@
 <body>
 <header>
 	<h1>Cookies.css<br>	<span><em>Easy-to-use cookiebars</em></span></h1>
+	<div class="buttons">
+		<a href="#" class="showNote">note</a>
+		<a href="#" class="showNote">notification</a>
+		<a href="#" class="showNote">triangle</a>
+		<a href="#" class="show-monster">monster</a>
+	</div>
 </header>
 <section class="first">
-	<div class="cookie note top">
+	<div class="cookie note top animated" id="close-note">
 		<div class="cookie--img">
 			<img src="assets/img/cookie-s.png" alt="cookie">
 		</div>
@@ -48,26 +69,25 @@
 		</div>
 	</div>
 
-	<div class="cookie notification bottom">
+	<div class="cookie notification bottom animated" >
 		<div class="cookie--text">
 			<p>Om je beter en persoonlijker te helpen, gebruiken wij cookies en vergelijkbare technieken. Met de cookies volgen wij en derde partijen jouw internetgedrag binnen onze site. Hiermee tonen we advertenties op basis van jouw interesse en kun je informatie delen via social media. Als je verdergaat op onze website gaan we ervan uit dat je dat goed vindt. Meer weten? <a href="#">Lees onze cookiepagina</a></p>
 		</div>
-		<div class="cookie--close">
+		<div class="cookie--close" id="close-notification">
 			<a href="#">Sluiten <span class="lnr lnr-cross"></span></a>
 		</div>
 	</div>
 
-	<div class="cookie triangle bottom">
+	<div class="cookie triangle bottom animated" >
 		<div class="cookie--text">
 			<p>Wij maken gebruik van cookies om uw surfervaring op deze website makkelijker te maken.<a href="#">Meer weten</a></p>
 		</div>
-		<div class="cookie--close">
+		<div class="cookie--close" id="close-triangle">
 			<a href="#"><span class="lnr lnr-cross"></span></a>
 		</div>
 	</div>
 
-	<div class="cookie monster">
-
+	<div class="cookie monster animated" >
 		<div class="cookie--text">
 			<p>Om je beter en persoonlijker te helpen, gebruiken wij cookies en vergelijkbare technieken. Met de cookies volgen wij en derde partijen jouw internetgedrag binnen onze site. Hiermee tonen we advertenties op basis van jouw interesse en kun je informatie delen via social media. Als je verdergaat op onze website gaan we ervan uit dat je dat goed vindt. Meer weten? <a href="#">Lees onze cookiepagina</a></p>
 		</div>
@@ -81,9 +101,20 @@
 
 
 </section>
-
-	<!-- foot scripts -->
-	<script src="assets/js/main.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.slim.min.js"></script>
+<script>
+$(document).ready(function(){
+    $(".cookie--close").click(function(){
+        $(".cookie").toggleClass("hidden",true);
+    });
+    $(".show-monster").click(function(){
+			$(".cookie").toggleClass("hidden",false);
+    });
+    $(".show-note").click(function(){
+			$(".cookie").toggleClass("hidden",false);
+    });
+});
+</script>
 
 </body>
 </html>
